f6ae0db6063774b1d0acede836305b5c7ed848e7
Geoffrey <geoolson@pdx.edu>
2020-03-22 14:26:00 -0700

    added json file

---
+++ src/svgmap/data/corona.js
@@ -0,0 +1,25 @@
+var svgMapDataGPD = {
+  data: {
+    totalCases: {
+      name: 'Total Cases',
+      format: '{0}',
+      thousandSeparator: ','
+    },
+    casesPerCapita: {
+      name: 'Cases per million people',
+      format: '{0}',
+      thousandSeparator: ','
+    },
+    active: {
+      name: 'Active Cases',
+      format: '{0}',
+      thousandSeparator: ','
+    },
+    newCases: {
+      name: 'New Cases',
+      format: '{0}',
+      thousandSeparator: ','
+    }
+  },
+  applyData: 'totalCases'
+}
